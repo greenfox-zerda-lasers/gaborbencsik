da9848daa3df11beec0a93f9b26dda40f3744f4c
Bencso <gabor.bencsik13@gmail.com>
2016-12-07 15:27:50 +0100

    exercise9

--- week-07/day-3/09.js
+++ week-07/day-3/09.js
@@ -4,3 +4,17 @@
 // it should return an object thats keys are the letters and the values are
 // the counts.
 // example: "apple" -> {a: 1, p: 2, l: 1, e: 1}
+
+function countLetters (stringInput) {
+  
+  return stringInput.split('').reduce(function(allCharacter, character) {
+    if (character in allCharacter) {
+      allCharacter[character]++;
+    } else {
+      allCharacter[character] = 1;
+    }
+    return allCharacter;
+  }, {});
+}
+
+console.log(countLetters("alma"))
