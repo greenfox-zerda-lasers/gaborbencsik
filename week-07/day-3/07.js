bfa853c480fa8c02dd7daca79a5a8e4157c03d21
Bencso <gabor.bencsik13@gmail.com>
2016-12-07 14:08:04 +0100

    exercise7

--- week-07/day-3/07.js
+++ week-07/day-3/07.js
@@ -4,3 +4,14 @@ var numbers = [2, 5, 11, 29];
 
 // create a function that takes an array of numbers and returns a boolean
 // it should return true if all the elements are prime, false otherwise
+
+function isPrime (number) {
+  for (var i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(numbers.every(isPrime));
