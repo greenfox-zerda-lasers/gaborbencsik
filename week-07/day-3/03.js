ea23e8c52816a0e10cc5c9c07a26226dfb27f88c
Bencso <gabor.bencsik13@gmail.com>
2016-12-07 11:38:04 +0100

    exercise3

--- week-07/day-3/03.js
+++ week-07/day-3/03.js
@@ -3,3 +3,11 @@
 // write a function called each that takes an array and a function as a paramter
 // and calls the function with each element of the array as parameter
 // so it should call the array 3 times if the array has 3 elements
+
+function each (arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+}
+
+each([1,2,3], console.log);
