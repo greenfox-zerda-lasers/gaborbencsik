4c1d24cd61dc40d46f0b6f72c8a5d62a70e7e0be
Bencso <gabor.bencsik13@gmail.com>
2016-12-07 15:57:01 +0100

    exercise10

--- week-07/day-3/10.js
+++ week-07/day-3/10.js
@@ -3,3 +3,21 @@
 // create a student object
 // that has a method `addgrade`, that takes a grade from 1 to 5
 // an other method `getAverage`, that returns the average of the grades
+
+var student = {
+  grades : [],
+  addGrade : function(grade) {
+    this.grades.push(grade);
+    return grade + ' added to grades';
+  },
+  getAverage : function(grades) {
+    return 'The avarage is: ' + this.grades.reduce(function (acc, item){
+      return acc + item;
+    }) / this.grades.length;
+  }
+}
+
+console.log(student.addGrade(3));
+console.log(student.addGrade(4));
+console.log(student.addGrade(4));
+console.log(student.getAverage());
