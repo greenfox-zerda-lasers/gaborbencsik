d3b1812a64efe605e7f5394897d65be1060cfdf0
Bencso <gabor.bencsik13@gmail.com>
2016-12-07 12:41:09 +0100

    exercise6

--- week-07/day-3/06.js
+++ week-07/day-3/06.js
@@ -3,3 +3,12 @@
 
 // create a function that takes a string and a letter and returns a boolean
 // it should return true if the string consits the given letter, false otherwise
+
+function listConsistLetter (string, letter) {
+  return string.split('').some(function(character) {
+    return letter === character;
+  });
+
+}
+
+console.log(listConsistLetter("alma","a"));
