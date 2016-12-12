88170352a37c7bf03bd421f324e2b36231fb4736
Bencso <gabor.bencsik13@gmail.com>
2016-12-12 11:53:23 +0100

    feat(week-08): 3.js completed

--- week-08/day-1/3.js
+++ week-08/day-1/3.js
@@ -7,3 +7,38 @@
 // the rocket with the amount of fuel given as a parameter
 // Every rocket should have a method called launch() that launches the rocket
 // if it has enough fuel (more than its consumption)
+
+function Rockets (consumption) {
+  this.consumption = consumption;
+  this.inTank = 0;
+  this.launches = 0;
+}
+
+Rockets.prototype.fill = function (amount) {
+  this.inTank += amount;
+  console.log(this.inTank + " fuel in tank");
+};
+
+Rockets.prototype.launch = function () {
+  if (this.inTank > this.consumption) {
+    this.launches++;
+    this.inTank -= this.consumption;
+    console.log(this.inTank + " fuel left.");
+    console.log("The Rocket has launched " + this.launches + "time(s)");
+  }
+};
+
+var rocket1 = new Rockets(3);
+var rocket2 = new Rockets(10);
+
+console.log(rocket1.consumption);
+console.log(rocket1.inTank);
+console.log(rocket1.launches);
+console.log(rocket2.consumption);
+console.log(rocket2.inTank);
+console.log(rocket2.launches);
+
+rocket1.fill(4);
+rocket2.fill(12);
+rocket1.launch();
+rocket2.launch();
