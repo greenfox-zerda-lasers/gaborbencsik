e14946e5dfd7f808c91fa097232a3cef4370cdde
Bencso <gabor.bencsik13@gmail.com>
2016-12-13 16:38:56 +0100

    feat(week-08): day-2 exercise 9

---
+++ week-08/day-2/9.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Create a function called ready
+//  - Embed this API endpoint as a <script> tag: https://api.github.com/orgs/greenfox-academy/repos?callback=ready
+//  - create the ready function
+//  - itearate through response.data array and console.log the name property
+
+function tibi (object) {
+  object.data.forEach(function(element) {
+    console.log(element.name);
+  })
+
+}
