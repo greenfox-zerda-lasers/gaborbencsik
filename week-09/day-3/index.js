6b8b0377631acdc58b0530a7c621acb618e1d245
Bencso <gabor.bencsik13@gmail.com>
2016-12-21 12:03:49 +0100

    1st exercise

---
+++ week-09/day-3/index.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',     /* ide kell tenni, hogy hol van a szerver*/
+  user: 'root',
+  password: '1988',
+  database: 'bookstore_2',
+});
+
+connection.connect(function(error) {
+  if (error) {
+    console.log('JAAAJ hiba', error);
+  } else {
+    console.log('Sikerült');
+  }
+});
+
+// connection.query("SELECT book_name FROM book_mast;",function(err,rows){
+//   if(err) {
+//     console.log(err.toString());
+//     return;
+//   }
+//
+//   console.log("Data received from Db:\n");
+//   console.log(rows);
+// });
+
+connection.query("SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast JOIN author ON book_mast.aut_id = author.aut_id JOIN  category ON book_mast.cate_id = category.cate_id JOIN publisher ON book_mast.pub_id = publisher.pub_id;",function(err,rows){
+  if(err) {
+    console.log(err.toString());
+    return;
+  }
+
+  console.log("Data received from Db:\n");
+  console.log(rows);
+});
+
+connection.end();
