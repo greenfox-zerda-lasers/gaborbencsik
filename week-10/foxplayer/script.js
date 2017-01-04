33b1ef6f8edeec5fdf679746130cc968bcae0229
Bencso <gabor.bencsik13@gmail.com>
2017-01-04 12:28:04 +0100

    feat(foxplayer): event listeners added

--- week-10/foxplayer/script.js
+++ week-10/foxplayer/script.js
@@ -1,49 +1,85 @@
+console.log('script is running')
+
 const addEvents = () => {
   let createPlaylist = document.querySelector('.create-playlist');
-  createPlaylist.addEventListener('click', createNewPlaylist());
+  createPlaylist.addEventListener('click', uiModule.createNewPlaylist);
   let allTracks = document.querySelector('.all-tracks');
-  allTracks.addEventListener('click', listAllTracks());
+  allTracks.addEventListener('click', uiModule.listAllTracks);
   let favoriteTracks = document.querySelector('.favorites');
-  favoriteTracks.addEventListener('click', listFavorites());
+  favoriteTracks.addEventListener('click', uiModule.listFavorites);
   let addSongToPlaylist = document.querySelector('.song-to-playlist');
-  addSongToPlaylist.addEventListener('click', addSongToList());
+  addSongToPlaylist.addEventListener('click', uiModule.addSongToList);
   let starIcon = document.querySelector('.ion-ios-star');
-  starIcon.addEventListener('click', addToFavorites());
+  starIcon.addEventListener('click', uiModule.addToFavorites);
 
   let rewind = document.querySelector('.ion-ios-rewind');
-  rewind.addEventListener('click', audioModule.rewindTrack();
+  rewind.addEventListener('click', audioModule.rewindSong);
   let play = document.querySelector('.ion-ios-play');
-  play.addEventListener('click', audioModule.playSong());
+  play.addEventListener('click', audioModule.playSong);
+  let forward = document.querySelector('.ion-ios-fastforward');
+  forward.addEventListener('click', audioModule.forwardSong);
+  let shuffle = document.querySelector('.ion-shuffle');
+  shuffle.addEventListener('click', audioModule.setRandomSong);
+  let mute = document.querySelector('.ion-android-volume-mute');
+  mute.addEventListener('click', audioModule.muteVolume);
+  let volume = document.querySelector('.volume-slider');
+  volume.addEventListener('click', audioModule.setVolume);
 }
 
 const audioModule = {
-  rewindTrack: function (event) {
+  rewindSong: function (event) {
     console.log(event);
+    console.log('rewind clicked');
   },
-  playSong:
-
+  playSong: function (event) {
+    console.log(event);
+    console.log('play clicked');
+  },
+  forwardSong: function (event) {
+    console.log(event);
+    console.log('forward clicked');
+  },
+  setRandomSong: function (event) {
+    console.log(event);
+    console.log('get random song');
+  },
+  muteVolume: function (event) {
+    console.log(event);
+    console.log('volume muted');
+  },
+  setVolume: function (event) {
+    console.log(event);
+    console.log('volume changed');
+  }
 }
 
 const uiModule = {
+  createNewPlaylist: function (event) {
+    console.log(event);
+    console.log('new playlist');
+  },
+
+  listAllTracks: function (event) {
+    console.log(event);
+    console.log('all tracks listed');
+  },
+
+  listFavorites: function (event) {
+    console.log(event);
+    console.log('favs listed');
+  },
+
+  addSongToList: function (event) {
+    console.log(event);
+    console.log('song added to a list');
+  },
+
+  addToFavorites: function (event) {
+    console.log(event);
+    console.log('song added to favs');
+  },
 
 }
 
-createNewPlaylist = function (event) {
-  console.log(event);
-}
 
-listAllTracks = function (event) {
-  console.log(event);
-}
-
-listFavorites = function (event) {
-  console.log(event);
-}
-
-addSongToList = function (event) {
-  console.log(event);
-}
-
-addToFavorites = function (event) {
-  console.log(event);
-}
+addEvents();
